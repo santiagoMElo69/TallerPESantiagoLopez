941dc8682fb9c5cebc66e55aa1b75b532d622c6c
miwwaw <santy.play444@gmail.com>
2026-02-05 18:50:04 -0500

    creacion de estudiantes base

--- taller/src/main/java/com/example/taller/controller/EstudianteController.java
+++ taller/src/main/java/com/example/taller/controller/EstudianteController.java
@@ -1,5 +1,40 @@
 package com.example.taller.controller;
 
+import com.example.taller.model.Estudiante;
+import com.example.taller.repository.EstudianteRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estudiantes")
+
 public class EstudianteController {
-    
+    private final EstudianteRepository repository;
+
+    public EstudianteController(EstudianteRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public String crearEstudiante(@RequestBody Estudiante estudiante) {
+        repository.save(estudiante);
+        return "Estudiante registrado correctamente";
+    }
+
+    @GetMapping
+    public List<Estudiante> listarEstudiantes() {
+        return repository.findAll();
+    }
+
+     @GetMapping("/test")
+    public String test() {
+        return "API estudiantes funcionando";
+    }
+
+    @GetMapping("/{id}")
+    public Estudiante buscarPorId(@PathVariable String id) {
+        return repository.findById(id);
+    }
+
 }
--- taller/src/main/java/com/example/taller/repository/EstudianteRepository.java
+++ taller/src/main/java/com/example/taller/repository/EstudianteRepository.java
@@ -1,5 +1,30 @@
 package com.example.taller.repository;
+import com.example.taller.model.Estudiante;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+@Repository
 public class EstudianteRepository {
-    
+
+    private List<Estudiante> estudiantes = new ArrayList<>();
+
+    public List<Estudiante> findAll() {
+        return estudiantes;
+    }
+
+    public void save(Estudiante estudiante) {
+        estudiantes.add(estudiante);
+    }
+
+    public Estudiante findById(String id) {
+        for (Estudiante e : estudiantes) {
+            if (e.getId().equals(id)) {
+                return e;
+            }
+        }
+        return null;
+    }
 }
+
+
